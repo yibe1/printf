3a43c5e462247ec1ddc3d7a5679e8f01e16192a9
yibe1 <yibe1481@gmail.com>
2022-08-11 16:27:33 +0300

    first commit for the group project _printf

--- _printf.c
+++ _printf.c
@@ -6,11 +6,12 @@
 #include <unistd.h>
 
 /**
- * _printf - prints anything
- * @format: list of argument types passed to the function
+ * _printf - print chracters
+ * @format: list of arg
  *
  * Return: number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
 unsigned int i = 0, count = 0;
@@ -54,17 +55,6 @@ unsigned int i;
 print_t p[] = {
 {"c", print_c},
 {"s", print_s},
-{"i", print_i},
-{"d", print_d},
-{"u", print_u},
-{"b", print_b},
-{"o", print_o},
-{"x", print_x},
-{"X", print_X},
-{"p", print_p},
-{"S", print_S},
-{"r", print_r},
-{"R", print_R},
 {NULL, NULL}
 };
 for (i = 0; p[i].t != NULL; i++)
