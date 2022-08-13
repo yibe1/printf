35aba7d53b2e451de17e3cff5329b029a7681858
yibe1 <yibe1481@gmail.com>
2022-08-13 17:32:19 +0300

    update on printf project

--- main.h
+++ main.h
@@ -1,22 +1,13 @@
 #ifndef MAIN_H_
 #define MAIN_H_
-#include <stdarg.h>
 /**
  * struct print - structur
  * @t: type to print
  * @f: function to print
  */
-typedef struct print
-{
-	char *t;
-	int (*f)(va_list);
-} print_t;
 
 int _putchar(char c);
 int _putnum(long int num);
 int _printf(const char *format, ...);
 int _printtext(char *ch);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
 #endif
